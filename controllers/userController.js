a9bffb4e02ebce270a186e07ef7900d01e0fa5b4
Jameson Wang <jamesonw10@gmail.com>
2023-04-15 19:47:01 -0400

    create user controller complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,45 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('Good.');
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please fill in all required fields');
+  }
+  if (password < 6) {
+    res.status(400);
+    throw new Error('Password must be more than 6 characters');
+  }
+
+  // Check if user email already exists
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error('Email is already in use');
+  }
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const { _id, name, email } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 module.exports = {
